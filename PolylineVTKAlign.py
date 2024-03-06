7b9130fdfae6936a7306454c2474367df11261db
Untuxed <lcm7498@gmail.com>
2024-03-06 14:15:44 -0500

    First release

--- PolylineVTKAlign.py
+++ PolylineVTKAlign.py
@@ -7,102 +7,129 @@ import natsort
 
 
 def angleBetweenVectors(v1, v2):
-    return np.arccos(
-            np.dot(v2, v1) /
-            (np.linalg.norm(v1) * np.linalg.norm(v2))
-            )
+    """
+    Calculates the angles between vectors
+    :param v1: three-dimensional vector
+    :param v2: three-dimensional vector
+    :return: angle in radians between two vectors
+    """
+    return np.arccos(np.dot(v2, v1) / (np.linalg.norm(v1) * np.linalg.norm(v2)))
 
 
 def rotatoChip(point, angle):
-    rotation_matrix = np.array([[1,             0,              0],
+    """
+    Rotates each point based on an angle. Assumes rotation about x-axis, to change you must change the rotation matrix
+    to the correct one. Was hungry when this function was made, now I think it's funny, so it is staying.
+    :param point: three-dimensional point
+    :param angle: angle b
+    :return: rotated three-dimensional point
+    """
+    rotationMatrixX = np.array([[1, 0, 0],
                                 [0, np.cos(angle), -np.sin(angle)],
                                 [0, np.sin(angle), np.cos(angle)]])
 
-    return np.dot(point, rotation_matrix)
+    return np.dot(point, rotationMatrixX)
 
 
+# Define input and output directory for files
+#### SHOULD ONLY NEED EDIT HERE ####
 in_directory = './testFiles/'
-filenames = natsort.natsorted(os.listdir(in_directory))
+outDirectory = './out_vtks/'
+if not os.path.isdir(outDirectory):
+    os.mkdir(outDirectory)
 
+# list all files in in_directory if they end with .vtk
+filenames = natsort.natsorted(os.listdir(in_directory))
 filenames = [filename for filename in filenames if filename.lower().endswith(".vtk")]
 
+# Define chosen location to translate the polylines to.
 origin = [0, 0, 0]
 
+# Define scale term and vtk reader and writer
 pclScaleLength = 0
+reader = vtk.vtkPolyDataReader()
+writer = vtk.vtkPolyDataWriter()
 
+# loop through files
 for filename in filenames:
-    reader = vtk.vtkPolyDataReader()
+    # loads vtk file
     reader.SetFileName(os.path.join(in_directory, filename))
     reader.Update()
 
+    # gets output data from vtk file
     data = vtk_to_numpy(reader.GetOutput().GetPoints().GetData())
 
+    # sets x-axis to zero and calculates flattened data
     data[:, 0] = 0
     flattenedDataLength = int(len(data) / 8) - 2
 
+    # Defines variables needed for scale and rotation loop
     new_data = np.empty((flattenedDataLength, 3))
-    prerotated_new_data = np.empty((flattenedDataLength, 3))
     j = 0
     cumulativeData = np.zeros(3)
 
+    # Define vtk parameters
     vtk_points = vtk.vtkPoints()
     polyLine = vtk.vtkCellArray()
     polyLine.InsertNextCell(flattenedDataLength)
 
+    # Loop through and compress data, this is an estimation of the center of the defined tube
     for i in range(len(data)):
-        cumulativeData = cumulativeData + data[i]
-        if i > len(data) - 15:
+        cumulativeData = cumulativeData + data[i]  # adds up 8 points
+        if i > len(data) - 15:  # break loop before it loops back to the beginning
             break
-        if (i + 1) % 8 == 0:
-            prerotated_new_data[j, :] = new_data[j, :] = cumulativeData / 8
+
+        if (i + 1) % 8 == 0:  # average the 8 points to get the middle of the tube
+            new_data[j, :] = cumulativeData / 8
             cumulativeData = np.zeros(3)
             j += 1
 
+    # transform data to the origin
     transform = origin - new_data[0]
-    if pclScaleLength == 0:
+    if pclScaleLength == 0:  # calculates base scale term for the files
         pclVector = new_data[0] - new_data[-1]
         pclScaleLength = np.linalg.norm(pclVector)
 
+    # transforms
     new_data[:, 1] += transform[1]
     new_data[:, 2] += transform[2]
 
+    # calculate difference between first and last point for scale and rotation
     pclVectorCurrent = new_data[0] - new_data[-1]
-
-    scale = pclScaleLength/np.linalg.norm(pclVectorCurrent)
-
+    scale = pclScaleLength / np.linalg.norm(pclVectorCurrent)
     new_data = new_data * scale
 
+    # calculates angle between the template shape and final shape
     angle_rads = angleBetweenVectors(pclVector, pclVectorCurrent)
 
+    # checks if the rotation has to be positive or negative
+    # this is a bad check, i should do this in the angle function
     if pclVector[2] > -1 * new_data[-1, 2]:
         angle_rads = -angle_rads
 
+    # rotates the data and assembles the new polylines
     for i in range(len(new_data)):
         new_data[i] = rotatoChip(new_data[i], angle_rads)
         vtk_points.InsertNextPoint(new_data[i])
         polyLine.InsertCellPoint(i)
 
+    # sets up the vtk data
     polyData = vtk.vtkPolyData()
     polyData.SetPoints(vtk_points)
     polyData.SetLines(polyLine)
 
+    # writes the vtk files
     newFilename = os.path.basename(filename)[:-4] + '_aligned.vtk'
-
-    outDirectory = './out_vtks/'
-
-    if not os.path.isdir(outDirectory):
-        os.mkdir(outDirectory)
-
-    writer = vtk.vtkPolyDataWriter()
     writer.SetFileName(os.path.join(outDirectory, newFilename))
     writer.SetFileVersion(42)
     writer.SetInputData(polyData)
-
     writer.Write()
 
+    # creates the plots
     plt.plot(new_data[:, 1], new_data[:, 2])
     pcl = [(0, 0), (new_data[-1, 1], new_data[-1, 2])]
     x, y = zip(*pcl)
     plt.plot(x, y)
 
+# plots data
 plt.show()
--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # VTK Data Alignment and Visualization
 
 ## Overview
-This Python script aligns and scales VTK polyline files, this code assumes that each of the files are polyline files on the ZY-plane. The first .vtk file in the specified folder is saved as the "template file" and all of the other files are rotated and scaled to match this file. It would be trivail to change this code to accomidate alignment to another plane, just be sure to change the rotation matrix in the function rotatochip. 
+This Python script aligns and scales VTK polyline files. The code assumes that each of the files is a polyline file on the ZY-plane, serving as a preliminary step for a principal component analysis (statistical shape model). The first .vtk file in the specified folder is saved as the "template file," and all the other files are rotated and scaled to match this file. It would be trivial to change this code to accommodate alignment to another plane; just be sure to modify the rotation matrix in the rotatochip function.
 
-Each file is saved in the output folder
+Each file is saved in the output folder (out_vtks), which is created in the installation directory in case it does not already exist. This code was written as part of the methods for an unpublished paper. The repository will be updated when the manuscript is published with a link to it.
+
+Seven example files demonstrating the code's functionality are included. Feel free to edit this code as needed for other projects.
 
 ## Requirements
 - Python 3.x
@@ -17,4 +19,29 @@ Install the required Python packages using the following command:
 pip install -r requirements.txt
 ```
 
+## Usage
+
+1. Point the script to the folder with your .vtk files
+2. Run the script
+
+The aligned vtk files are saved in ./out_vtks/ with the suffix "_aligned" by default
+
+## Functions
+
+`angleBetweenVectors(v1, v2)`
+Calculates the angle between the two vectors in radians.
+
+`rotatoChip(point, angle)`
+Rotates each point passed in around the x-axis by the specified angle.
+
+## Main Loop
+
+1. Reads the .vtk's from specified directory
+2. Translates the first point of the polylines to (0, 0)
+3. Processes and aligns each of the polyLines based on the vector between the first and last point
+4. Shows a plot of all the polylines as a check
+
+## Notes
+
+The author of this script only guarantee its accuracy for the specific use case outlined above. If a user of this script need to consider the positional data for their data this script will distort that. 
 
